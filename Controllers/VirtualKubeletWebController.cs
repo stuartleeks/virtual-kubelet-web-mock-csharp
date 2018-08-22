aa3080ca520440a5f4d233d8eddf94c682a92506
Stuart Leeks <stuartle@microsoft.com>
2018-08-22 15:16:51 +0100

    Add nodeConditions

--- Controllers/VirtualKubeletWebController.cs
+++ Controllers/VirtualKubeletWebController.cs
@@ -17,6 +17,7 @@ namespace vk_web_mock.Controllers
         {
             _logger = logger;
         }
+
         [HttpGet("capacity")]
         public IActionResult GetCapacity()
         {
@@ -24,6 +25,63 @@ namespace vk_web_mock.Controllers
                 cpu = "20",
                 memory = "100Gi",
                 pods ="20"
+            });
+        }
+
+        [HttpGet("nodeConditions")]
+        public IActionResult GetNodeConditions(){
+            DateTime utcNow = DateTime.UtcNow;
+
+            return base.Json(new [] {
+                new {
+                    lastHeartbeatTime = utcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                    lastTransitionTime = DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"),
+                    message= "At your service",
+                    reason = "KubeletReady",
+                    status = "True",
+                    type="Ready"
+                }
+
+        //         {
+		// 	Type:               "Ready",
+		// 	Status:             v1.ConditionTrue,
+		// 	LastHeartbeatTime:  metav1.Now(),
+		// 	LastTransitionTime: metav1.Now(),
+		// 	Reason:             "KubeletReady",
+		// 	Message:            "kubelet is ready.",
+		// },
+		// {
+		// 	Type:               "OutOfDisk",
+		// 	Status:             v1.ConditionFalse,
+		// 	LastHeartbeatTime:  metav1.Now(),
+		// 	LastTransitionTime: metav1.Now(),
+		// 	Reason:             "KubeletHasSufficientDisk",
+		// 	Message:            "kubelet has sufficient disk space available",
+		// },
+		// {
+		// 	Type:               "MemoryPressure",
+		// 	Status:             v1.ConditionFalse,
+		// 	LastHeartbeatTime:  metav1.Now(),
+		// 	LastTransitionTime: metav1.Now(),
+		// 	Reason:             "KubeletHasSufficientMemory",
+		// 	Message:            "kubelet has sufficient memory available",
+		// },
+		// {
+		// 	Type:               "DiskPressure",
+		// 	Status:             v1.ConditionFalse,
+		// 	LastHeartbeatTime:  metav1.Now(),
+		// 	LastTransitionTime: metav1.Now(),
+		// 	Reason:             "KubeletHasNoDiskPressure",
+		// 	Message:            "kubelet has no disk pressure",
+		// },
+		// {
+		// 	Type:               "NetworkUnavailable",
+		// 	Status:             v1.ConditionFalse,
+		// 	LastHeartbeatTime:  metav1.Now(),
+		// 	LastTransitionTime: metav1.Now(),
+		// 	Reason:             "RouteCreated",
+		// 	Message:            "RouteController created a route",
+		// },
             });
         }
 
