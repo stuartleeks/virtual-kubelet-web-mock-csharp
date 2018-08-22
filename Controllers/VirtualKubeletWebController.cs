9a2fbbdca95456535e6fb9e42f13fdf02fb2b710
Stuart Leeks <stuartle@microsoft.com>
2018-08-22 14:20:54 +0100

    Add initial api controller with fixed capacity endpoint

---
+++ Controllers/VirtualKubeletWebController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using vk_web_mock.Models;
+
+namespace vk_web_mock.Controllers
+{
+    [ApiController]
+    public class VirtualKubeletWebController : Controller
+    {
+        [HttpGet("capacity")]
+        public IActionResult GetCapacity()
+        {
+            return Json(new {
+                cpu = "20",
+                memory = "100Gi",
+                pods ="20"
+            });
+        }
+    }
+}
